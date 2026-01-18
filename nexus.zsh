00c50986c7c4a0f051b060b0e80bc092958663a8
Vinod.Muleva <muleva97vinod@gmail.com>
2026-01-18 11:09:04 +0530

    Enforce LF line endings for shell scripts

---
+++ nexus.zsh
@@ -0,0 +1,4 @@
+Hey There my Boy How are you ,doing 
+
+
+Is there any Work to do with you , or are you Free today ?..
---
+++ testing.py
@@ -0,0 +1,1 @@
+print("Hello Dosto , Kya haal Chal , kaise hai sabb log ?.. I am Vinod Muleva from Mumbai A Software Engineer")
